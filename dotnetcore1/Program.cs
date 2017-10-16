a99e65f43538b3b1299d20d3fd2ffc285b4794a9
Gene Maltepes <gene.maltepes@gmail.com>
2017-10-16 20:30:35 +0000

    first commit

---
+++ dotnetcore1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
